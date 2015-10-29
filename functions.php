854924a8c4ab96005a673006e032dbf8bbcade14
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2015-10-29 21:58:37 +0900

    p269まで済み

--- functions.php
+++ functions.php
@@ -12,3 +12,24 @@ add_theme_support('automatic-feed-links');
 
 // カスタムメニュー
 register_nav_menu('navigation', 'ナビゲーション');
+
+// カスタムヘッダー
+add_theme_support('custom-header',
+	array('width' => 1500,
+				'height' => 250));
+
+// 概要(抜粋)の文字数
+function my_length($length){
+	return 70;
+}
+
+add_filter('excerpt_mblength', 'my_length');
+
+function my_more($more){
+	return '･･･';
+}
+
+add_filter('excerpt_more', 'my_more');
+
+// アイキャッチ画像
+add_theme_support('post-thumbnails');
